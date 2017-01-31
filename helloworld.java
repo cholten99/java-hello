5d294d25dfbe0ef2cdf52e8dcdb9f314b46c1a00
David Durant <dave@bowsy.co.uk>
2017-01-31 01:50:02 +0000

    Only the best merges

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,17 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+
+What's this line below this one?
+
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World";
     }
+
+Oh yeah, and how do
+multi-line
+comments
+work
+again?
 
 }
